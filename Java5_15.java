2f2912ef278e9a700e9fcd2513f12d286118ad72
Infernolia <47828335+Infernolia@users.noreply.github.com>
2021-01-28 00:16:15 +0530

    Add files via upload

---
+++ Java5_15.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+class Demo
+		{
+			public static void main(String args[]) 
+				{
+				fun();
+				
+					
+					
+				}
+
+			static void fun()
+			{
+
+			fun();
+			}
+		
+
+
+
+			
+						
+		}
