cc03a9da32d528082654bedd2b2d0253a022a3a1
Infernolia <47828335+Infernolia@users.noreply.github.com>
2021-01-28 00:17:07 +0530

    Add files via upload

---
+++ Java5_17.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Demo
+{
+public static void main(String args[])throws Exception,Throwable
+{
+Object obj = new Integer(10);
+String str = (String)obj;
+System.out.println(""+str);
+
+
+}
+}
