7a2c41b5a8460676416b560e5bf4b818220daa7d
Infernolia <47828335+Infernolia@users.noreply.github.com>
2021-01-28 00:16:38 +0530

    Add files via upload

---
+++ Java5_16.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+class Demo
+		{
+			public static void main(String args[]) throws IOException
+				{
+				
+				InputStreamReader isr;
+				isr = new InputStreamReader(System.in);
+				BufferedReader bfr = new BufferedReader(isr);
+				char ch;
+				
+				int x = (int)isr.read();
+				System.out.println(""+x);
+
+				/*
+				String an = (String)bfr.readLine();
+				System.out.println(an);
+				ch = (char)bfr.read();
+				
+				System.out.println(ch);
+				*/
+					
+					
+				}
+
+			
+		
+
+
+
+			
+						
+		}
