590218d0d6b077f8f8f5709f8c885a940316cdc7
Infernolia <47828335+Infernolia@users.noreply.github.com>
2021-01-28 00:13:59 +0530

    Add files via upload

---
+++ Java5_08.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+
+
+
+
+
+class Practice1 
+		{
+			public static void main(String args[]) throws Exception
+				{
+					
+					System.out.println(" In main here");
+
+					
+				}
+						
+		}
+
+
+class Practice2 
+		{
+			public static void main(String args[]) throws Exception
+				{
+					
+					Practice1 obj = new Practice1();
+					String str[] = {"Hi","Bye"};
+					obj.main(str);
+					
+				}
+						
+		}
