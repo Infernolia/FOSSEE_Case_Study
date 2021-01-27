188fa24edefbafcce6864823fadb39e8b71d7924
Infernolia <47828335+Infernolia@users.noreply.github.com>
2021-01-28 00:15:57 +0530

    Add files via upload

---
+++ Java5_14.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+class Demo
+		{
+			public static void main(String args[])throws ArithmeticException
+				{
+				System.out.println(10/0);
+				
+					
+					
+				}
+		
+
+
+
+			
+						
+		}
