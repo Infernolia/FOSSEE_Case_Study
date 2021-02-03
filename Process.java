e7cdb8a02e170116e23363afcfc496b9c9a851b6
Infernolia <47828335+Infernolia@users.noreply.github.com>
2021-02-04 00:28:19 +0530

    Add files via upload

---
+++ Process.java
@@ -0,0 +1,17 @@
+
+public class Process {
+	
+	int pid;
+	int bt;
+	int at;
+	
+	public Process(int pid, int at, int bt)
+	{
+
+        this.pid = pid; 
+        this.bt = bt; 
+        this.at = at;
+        
+	}
+
+}
