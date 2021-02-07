7810fe7a128e04e41dbcc942246f69c556c2086c
Infernolia <47828335+Infernolia@users.noreply.github.com>
2021-02-08 00:32:45 +0530

    Add files via upload

--- data.py
+++ data.py
@@ -1,303 +1,70 @@
-import tensorflow as tf
-from tensorflow import keras
 
+from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
+import torch
+import pandas as pd
 import numpy as np
-import os
-import cv2
 
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from pathlib import Path
 
-def _process_csv_file(file):
-    with open(file, 'r') as fr:
-        files = fr.readlines()
-    return files
+def split_data(file_path, destination):
 
-class BalanceCovidDataset(keras.utils.Sequence):
-    'Generates data for Keras'
+    data = pd.read_csv(file_path, sep='\t', header=None)[[0, 1]]
 
-    def __init__(
-            self,
-            data_dir,
-            csv_file,
-            is_training=True,
-            batch_size=8,
-            input_shape=(224, 224),
-            n_classes=3,
-            num_channels=3,
-            mapping={
-                'normal': 0,
-                'pneumonia': 1,
-                'COVID-19': 2
-            },
-            shuffle=True,
-            augmentation=True,
-            covid_percent=0.3,
-            class_weights=[1., 1., 6.]
-    ):
-        'Initialization'
-        self.datadir = data_dir
-        self.dataset = _process_csv_file(csv_file)
-        self.is_training = is_training
-        self.batch_size = batch_size
-        self.N = len(self.dataset)
-        self.input_shape = input_shape
-        self.n_classes = n_classes
-        self.num_channels = num_channels
-        self.mapping = mapping
-        self.shuffle = True
-        self.covid_percent = covid_percent
-        self.class_weights = class_weights
-        self.n = 0
+    mask = np.random.rand(data.shape[0]) < 0.8
+    train = data[mask]
+    valid = data[~mask]
 
-        if augmentation:
-            self.augmentation = ImageDataGenerator(
-                featurewise_center=False,
-                featurewise_std_normalization=False,
-                rotation_range=10,
-                width_shift_range=0.1,
-                height_shift_range=0.1,
-                horizontal_flip=True,
-                brightness_range=(0.9, 1.1),
-                zoom_range=(0.85, 1.15),
-                fill_mode='constant',
-                cval=0.,
-            )
+    train.to_csv(Path(destination) / 'train', header=False, index=False)
+    valid.to_csv(Path(destination) / 'valid', header=False, index=False)
 
-        datasets = {'normal': [], 'pneumonia': [], 'COVID-19': []}
-        for l in self.dataset:
-            datasets[l.split()[-1]].append(l)
-        self.datasets = [
-            datasets['normal'] + datasets['pneumonia'],
-            datasets['COVID-19'],
-        ]
-        print(len(self.datasets[0]), len(self.datasets[1]))
 
-        self.on_epoch_end()
 
-    def __next__(self):
-        # Get one batch of data
-        batch_x, batch_y, weights = self.__getitem__(self.n)
-        # Batch index
-        self.n += 1
+class PadSequence:
+    
+    def __init__(self, src_padding_value, tgt_padding_value):
+        self.src_padding_value = src_padding_value
+        self.tgt_padding_value = tgt_padding_value
+    
+    def __call__(self, batch):
+        
+        x = [s[0] for s in batch]
+        x = pad_sequence(x, 
+                         batch_first=True, 
+                         padding_value=self.src_padding_value)
 
-        # If we have processed the entire dataset then
-        if self.n >= self.__len__():
-            self.on_epoch_end
-            self.n = 0
+        y = [s[1] for s in batch]
+        y = pad_sequence(y, 
+                         batch_first=True, 
+                         padding_value=self.tgt_padding_value)
 
-        return batch_x, batch_y, weights
+        return x, y
+
+
+class IndicDataset(Dataset):
+  
+    def __init__(self, 
+                 src_tokenizer,
+                 tgt_tokenizer,
+                 filepath,
+                 is_train=True):
+        filepath += 'train' if is_train else 'valid'
+        self.df = pd.read_csv(filepath, header=None)
+
+        self.src_tokenizer = src_tokenizer
+        self.tgt_tokenizer = tgt_tokenizer
 
     def __len__(self):
-        return int(np.ceil(len(self.datasets[0]) / float(self.batch_size)))
+        return self.df.shape[0]
 
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        if self.shuffle == True:
-            for v in self.datasets:
-                np.random.shuffle(v)
+    def __getitem__(self, index):
+        y, x = self.df.loc[index]
+ 
+        #tokenize into integer indices
+        x = self.src_tokenizer.convert_tokens_to_ids(self.src_tokenizer.tokenize(x))
+        y = self.tgt_tokenizer.convert_tokens_to_ids(self.tgt_tokenizer.tokenize(y))
 
-    def __getitem__(self, idx):
-        batch_x, batch_y = np.zeros(
-            (self.batch_size, *self.input_shape,
-             self.num_channels)), np.zeros(self.batch_size)
+        #add special tokens to target
+        y = [self.tgt_tokenizer.bos_token_id] + y + [self.tgt_tokenizer.eos_token_id]
 
-        batch_files = self.datasets[0][idx * self.batch_size:(idx + 1) *
-                                       self.batch_size]
-
-        # upsample covid cases
-        covid_size = max(int(len(batch_files) * self.covid_percent), 1)
-        covid_inds = np.random.choice(np.arange(len(batch_files)),
-                                      size=covid_size,
-                                      replace=False)
-        covid_files = np.random.choice(self.datasets[1],
-                                       size=covid_size,
-                                       replace=False)
-        for i in range(covid_size):
-            batch_files[covid_inds[i]] = covid_files[i]
-
-        for i in range(len(batch_files)):
-            sample = batch_files[i].split()
-
-            if self.is_training:
-                folder = 'train'
-            else:
-                folder = 'test'
-
-            x = cv2.imread(os.path.join(self.datadir, folder, sample[1]))
-            h, w, c = x.shape
-            x = x[int(h/6):, :]
-            x = cv2.resize(x, self.input_shape)
-
-            if self.is_training and hasattr(self, 'augmentation'):
-                x = self.augmentation.random_transform(x)
-
-            x = x.astype('float32') / 255.0
-            y = self.mapping[sample[2]]
-
-            batch_x[i] = x
-            batch_y[i] = y
-
-        class_weights = self.class_weights
-        weights = np.take(class_weights, batch_y.astype('int64'))
-
-        return batch_x, keras.utils.to_categorical(batch_y, num_classes=self.n_classes), weights
-
-class BalanceDataGenerator(keras.utils.Sequence):
-    'Generates data for Keras'
-    def __init__(self,
-                 dataset,
-                 is_training=True,
-                 batch_size=8,
-                 input_shape=(224,224),
-                 n_classes=3,
-                 num_channels=3,
-                 mapping={'normal': 0, 'pneumonia': 1, 'COVID-19': 2},
-                 shuffle=True,
-                 augmentation=True,
-                 datadir='data',
-                 class_weights=[1., 1., 25.]
-                 ):
-        'Initialization'
-        self.datadir = datadir
-        self.dataset = dataset
-        self.is_training = is_training
-        self.batch_size = batch_size
-        self.N = len(self.dataset)
-        self.input_shape = input_shape
-        self.n_classes = n_classes
-        self.num_channels = num_channels
-        self.mapping = mapping
-        self.shuffle = True
-        self.n = 0
-        self.class_weights = class_weights
-
-        if augmentation:
-            self.augmentation = ImageDataGenerator(
-                featurewise_center=False,
-                featurewise_std_normalization=False,
-                rotation_range=10,
-                width_shift_range=0.1,
-                height_shift_range=0.1,
-                horizontal_flip=True,
-                brightness_range=(0.9, 1.1),
-                fill_mode='constant',
-                cval=0.,
-            )
-
-        datasets = {'normal': [], 'pneumonia': [], 'COVID-19': []}
-        for l in dataset:
-            datasets[l.split()[-1]].append(l)
-        self.datasets = [
-            datasets['normal'] + datasets['pneumonia'],
-            datasets['COVID-19'],
-        ]
-        print(len(self.datasets[0]), len(self.datasets[1]))
-
-        self.on_epoch_end()
-
-    def __next__(self):
-        # Get one batch of data
-        batch_x, batch_y, weights = self.__getitem__(self.n)
-        # Batch index
-        self.n += 1
-
-        # If we have processed the entire dataset then
-        if self.n >= self.__len__():
-            self.on_epoch_end
-            self.n = 0
-
-        return batch_x, batch_y, weights
-
-    def __len__(self):
-        return int(np.ceil(len(self.datasets[0]) / float(self.batch_size)))
-
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        if self.shuffle == True:
-            for v in self.datasets:
-                np.random.shuffle(v)
-
-    def __getitem__(self, idx):
-        batch_x, batch_y = np.zeros((self.batch_size, *self.input_shape, self.num_channels)), np.zeros(self.batch_size)
-
-        batch_files = self.datasets[0][idx*self.batch_size : (idx+1)*self.batch_size]
-        batch_files[np.random.randint(self.batch_size)] = np.random.choice(self.datasets[1])
-
-        for i in range(self.batch_size):
-            sample = batch_files[i].split()
-
-            if self.is_training:
-                folder = 'train'
-            else:
-                folder = 'test'
-
-            x = cv2.imread(os.path.join(self.datadir, folder, sample[1]))
-            h, w, c = x.shape
-            x = x[int(h/6):, :]
-            x = cv2.resize(x, self.input_shape)
-
-            if self.is_training and hasattr(self, 'augmentation'):
-                x = self.augmentation.random_transform(x)
-
-            x = x.astype('float32') / 255.0
-            y = self.mapping[sample[2]]
-
-            batch_x[i] = x
-            batch_y[i] = y
-
-        weights = np.take(self.class_weights, batch_y.astype('int64'))
-
-        return batch_x, keras.utils.to_categorical(batch_y, num_classes=self.n_classes), weights
-
-
-class DataGenerator(keras.utils.Sequence):
-    'Generates data for Keras'
-    def __init__(self,
-                 dataset,
-                 is_training=True,
-                 batch_size=8,
-                 input_shape=(224,224),
-                 n_classes=3,
-                 num_channels=3,
-                 mapping={'normal': 0, 'pneumonia': 1, 'COVID-19': 2},
-                 shuffle=True):
-        'Initialization'
-        self.dataset = dataset
-        self.is_training = is_training
-        self.batch_size = batch_size
-        self.N = len(self.dataset)
-        self.input_shape = input_shape
-        self.n_classes = n_classes
-        self.num_channels = num_channels
-        self.mapping = mapping
-        self.shuffle = True
-        self.on_epoch_end()
-
-    def __len__(self):
-        return int(np.ceil(self.N / float(self.batch_size)))
-
-    def on_epoch_end(self):
-        self.dataset = shuffle(self.dataset, random_state=0)
-
-    def __getitem__(self, idx):
-        batch_x, batch_y = np.zeros((self.batch_size, *self.input_shape, self.num_channels)), np.zeros(self.batch_size)
-        for i in range(self.batch_size):
-            index = min((idx * self.batch_size) + i, self.N-1)
-
-            sample = self.dataset[index].split()
-
-            if self.is_training:
-                folder = 'train'
-            else:
-                folder = 'test'
-
-            x = cv2.imread(os.path.join('data', folder, sample[1]))
-            x = cv2.resize(x, self.input_shape)
-            x = x.astype('float32') / 255.0
-            #y = int(sample[1])
-            y = self.mapping[sample[2]]
-
-            batch_x[i] = x
-            batch_y[i] = y
-
-        return batch_x, keras.utils.to_categorical(batch_y, num_classes=self.n_classes)
+        return torch.LongTensor(x), torch.LongTensor(y)
