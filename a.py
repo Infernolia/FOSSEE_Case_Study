a6226254a795ac9773dc20a8f54e71cc0ecf79ff
Infernolia <47828335+Infernolia@users.noreply.github.com>
2021-02-23 00:05:38 +0530

    Add files via upload

--- a.py
+++ a.py
@@ -1,12 +1,32 @@
-import cudf; print('cuDF Version:', cudf.__version__)
-
-
-# here we create a cuDF DataFrame with
-# two columns named "key" and "value"
-df = cudf.DataFrame()
-df['key'] = [0, 0, 2, 2, 3]
-df['value'] = [float(i + 10) for i in range(5)]
-df
-
-aggregation = df['value'].sum()
-print(aggregation)
+import cv2 
+import numpy as np 
+  
+# Let's load a simple image with 3 black squares 
+image = cv2.imread('C:/User/Aboli/Desktop/a.png') 
+cv2.waitKey(0) 
+  
+# Grayscale 
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+  
+# Find Canny edges 
+edged = cv2.Canny(gray, 30, 200) 
+cv2.waitKey(0) 
+  
+# Finding Contours 
+# Use a copy of the image e.g. edged.copy() 
+# since findContours alters the image 
+contours, hierarchy = cv2.findContours(edged,  
+    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
+  
+cv2.imshow('Canny Edges After Contouring', edged) 
+cv2.waitKey(0) 
+  
+print("Number of Contours found = " + str(len(contours))) 
+  
+# Draw all contours 
+# -1 signifies drawing all contours 
+cv2.drawContours(image, contours, -1, (0, 255, 0), 3) 
+  
+cv2.imshow('Contours', image) 
+cv2.waitKey(0) 
+cv2.destroyAllWindows() 
